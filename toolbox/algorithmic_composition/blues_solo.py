7d0e0d08bef52cde57f892ba82c56f08d7c303a8
rdiverdi <Rocco.DiVerdi@students.olin.edu>
2015-03-20 21:50:59 -0400

    completed algorithmic composition toolbox

--- toolbox/algorithmic_composition/blues_solo.py
+++ toolbox/algorithmic_composition/blues_solo.py
@@ -31,6 +31,38 @@ solo = AudioStream(sampling_rate, 1)
 blues_scale = [25, 28, 30, 31, 32, 35, 37, 40, 42, 43, 44, 47, 49, 52, 54, 55, 56, 59, 61]
 beats_per_minute = 45				# Let's make a slow blues solo
 
-add_note(solo, bass, blues_scale[0], 1.0, beats_per_minute, 1.0)
+vol = 1.0
 
-solo >> "blues_solo.wav"
+curr_note = 6
+
+licks = [[ [0, 0.6, 0], [1,0.4, 0.1], [1, 0.6, 0.1], [1, 0.4, 0.1] ],
+         [ [0, 0.6, -0.1], [-1,0.4, -0.1], [-1, 0.6, -0.1], [-1, 0.4, -0.1] ],
+         [ [0, 0.5, 0], [2, 0.25, 0.3], [-1, 0.5, -0.2], [2, 0.25, -0.1], [-1, 0.5, 0.1] ],
+         [ [0, 0.6, 0.1], [2, 0.4, 0.2], [-1, 1.0, -0.1] ]]
+length = 16
+half_len = length/2.
+max_vol = 2.
+mult = max_vol / (half_len**2)
+
+for i in range(length):
+    lick = choice(licks)
+    for note in lick:
+        curr_note = (curr_note + note[0]) % (len(blues_scale) - 1)
+        add_note(solo, bass, blues_scale[curr_note], note[1], beats_per_minute, vol)
+    vol = max_vol-mult * (i-half_len)**2 +1
+    curr_note = choice([6, 6, 12, 12, 5, 7, 11, 13])
+
+add_note(solo, bass, blues_scale[curr_note], 1.0, beats_per_minute, vol)
+
+backing_track = AudioStream(sampling_rate, 1)
+Wavefile.read('backing.wav', backing_track)
+
+m = Mixer()
+
+solo *= 0.7             # adjust relative volumes to taste
+backing_track *= 1.5
+
+m.add(2.25, 0, solo)    # delay the solo to match up with backing track
+m.add(0, 0, backing_track)
+
+m.getStream(500.0) >> "slow_blues.wav"
